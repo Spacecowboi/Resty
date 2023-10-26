860d5e149b76c1757475a060edeec01c881c0446
Spacecowboi <cunningham2192@gmail.com>
2023-10-25 20:24:13 -0700

    updated tests

--- resty/src/Components/Form/Form.test.jsx
+++ resty/src/Components/Form/Form.test.jsx
@@ -8,12 +8,20 @@ test('Form component without crashing', () => {
     const { getByText, getByLabelText } = render(<Form handleApiCall={handleApiCall} />)
 
     expect(getByLabelText('URL:')).toBeInTheDocument()
+    expect(getByLabelText('Body:')).toBeInTheDocument() // New test for Body input
     expect(getByText('GO!')).toBeInTheDocument()
     expect(getByText('GET')).toBeInTheDocument()
     expect(getByText('POST')).toBeInTheDocument()
     expect(getByText('PUT')).toBeInTheDocument()
     expect(getByText('DELETE')).toBeInTheDocument()
+
+    fireEvent.change(getByLabelText('URL:'), { target: { value: 'https://test.com' } })
+    fireEvent.change(getByLabelText('Body:'), { target: { value: 'Test body' } }) // New test for Body input
     fireEvent.click(getByText('GO!'))
-  
-    expect(formData).not.toBeNull()
-  })
+
+    expect(formData).toEqual({
+        method: 'GET',
+        url: 'https://test.com',
+        body: 'Test body',
+    })
+})
